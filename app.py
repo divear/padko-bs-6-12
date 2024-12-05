ba81f6fb668e4421df71fad808ac03037789640a
divear <lukyn.odehnal333@gmail.com>
2024-12-06 00:31:03 +0100

    odpoved checking

--- app.py
+++ app.py
@@ -4,10 +4,18 @@ class Test:
     def __init__(self):
         self.vse = "Všobecné znalosti"
         self.datum = datetime.now().strftime("%Y-%m-%d")
-        self.odpovedi = []
+        self.odpovedi = [
+            "Lisabon",
+            "30 let",
+            "144",
+            "8 bitů",
+            "Isaac Asimov"
+        ]
+        self.pointy = 0
+
 
     def typ_testu(self):
-        moznost = input("Ktery typ testu chcete (T: Test P: Přijímačky)? ")
+        moznost = input("Ktery typ testu chcete (T: Test P: Přijímačky)? ").upper()
         if moznost == "T":
             print("test")
         elif moznost == "P":
@@ -18,6 +26,9 @@ class Test:
         self.trida = input("Vase trida: ")
         self.predmet =  "matika idk"
         return f"Pocatecni informace: \n predmet: {self.predmet} \n datum: {self.datum}\nJmeno a prijimeni: {self.jmeno}\ntrida: {self.trida}"
+    def kontrola(self, odpoved, index):
+            if odpoved == self.odpovedi[index]:
+                self.pointy+=1
     def otazky(self):
         self.otazky = [
             "Hlavní město Portugalska?",
@@ -26,7 +37,21 @@ class Test:
             "1 Byte = ? bitů?",
             "Autor díla Já, robot?"
         ]
+        for (i,e) in enumerate(self.otazky):
+            odpoved = input(f"\n{e} ")
+            self.kontrola(odpoved, i)
+        print(self.pointy)
+
+
 
 
 d = Test()
-d.typ_testu()
+d.otazky()
+
+
+# Co chybi:
+# b) tohle mozna mam ale idk radsi double check
+# f) idk co really chce
+# g) hodnoceni
+# h) destruktor
+# cela dvojka a trojka xd
